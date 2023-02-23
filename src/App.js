faaccf417ee1fc8050eb2ea19e69e78bc08a4447
ishani <91791452+deathmukh@users.noreply.github.com>
2023-02-23 21:40:06 +0530

    added registration page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Register from "./Pages/Register/Register";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
   return (
     <div className="App">
       <Register />
+      {/* <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        </Routes>
+        </BrowserRouter> */}
     </div>
   );
 }
